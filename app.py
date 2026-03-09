80275f31b308bcde6bd363e9d1e2770416d3f1b1
Kipyegon Wilber <kenmisik@gmail.com>
2026-03-09 18:55:30 +0300

    use DATABASE_URL for render postgres

--- app.py
+++ app.py
@@ -18,7 +18,8 @@ def get_data(query):
         user=os.getenv("DB_USER"),
         password=os.getenv("DB_PASSWORD"),
         host=os.getenv("DB_HOST"),
-        port=os.getenv("DB_PORT")
+        port=os.getenv("DB_PORT"),
+        database_url=os.getenv("DATABASE_URL")
     )
     df = pd.read_sql(query, conn)
     conn.close()
